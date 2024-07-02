5cb97b7973493ac2afb94bb6bb2da5a1f8328de9
sureshkumar.thiyagarajan <sureshkumar.thiyagarajan@earlywarning.com>
2024-07-01 17:46:26 -0700

    Init Commit

--- OrderService/src/main/java/com/code2learn/opentelemetry/configuration/RestConfiguration.java
+++ OrderService/src/main/java/com/code2learn/opentelemetry/configuration/RestConfiguration.java
@@ -1,7 +1,5 @@
 package com.code2learn.opentelemetry.configuration;
 
-import io.opentelemetry.api.OpenTelemetry;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
